301d3a68d28807970b4c967abb3240fa42083fb3
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 00:09:32 +0600

    Make 3 button on the Home.js file for demonstrating event handling.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log('Hello!');
+    }
+    function handleClick2(name) {
+        console.log(`Hello ${name}!`);
+    }
+    function handleClick3(e) {
+        console.log(e.target);
+    }
+    
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>My Button</button>
+            <button onClick={() => handleClick2('Mario')}>My Button 2</button>
+            <button onClick={handleClick3}>My Button 3</button>
         </div>
     );
 }
