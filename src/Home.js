ace3ab43a10c372796a1f983d7e7379b57b67cb5
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 00:48:43 +0600

    Add some blogs to the homepage

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
-const Home = () => {
+import { useState } from "react";
+
+export default function Home() {
+    const [blogs, setBlogs] = useState([
+        { id: 999991, title: "My new website", author: "Mario", body: "Lorem ipsum..." },
+        { id: 999992, title: "My new website", author: "Mario", body: "Lorem ipsum..." },
+        { id: 999993, title: "My new website", author: "Mario", body: "Lorem ipsum..." },
+        { id: 999994, title: "My new website", author: "Mario", body: "Lorem ipsum..." },
+        { id: 999995, title: "My new website", author: "Mario", body: "Lorem ipsum..." }
+    ]);
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            { blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            )) }
         </div>
     );
 }
- 
-export default Home;
