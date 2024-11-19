1ff1e37e86a2c8ed27f2afafff96138649cf9120
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 01:24:39 +0600

    Add another BlogLists in Home.js for demonstating resuing component.

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,16 @@ import BlogLists from "./BlogLists";
 export default function Home() {
     const [blogs, setBlogs] = useState([
         { id: 999991, title: "My new website", author: "Mario", body: "Lorem ipsum..." },
-        { id: 999992, title: "My new website", author: "Mario", body: "Lorem ipsum..." },
-        { id: 999993, title: "My new website", author: "Mario", body: "Lorem ipsum..." },
-        { id: 999994, title: "My new website", author: "Mario", body: "Lorem ipsum..." },
-        { id: 999995, title: "My new website", author: "Mario", body: "Lorem ipsum..." }
+        { id: 999992, title: "Super market day", author: "Yushi", body: "Lorem ipsum..." },
+        { id: 999993, title: "The day of doone", author: "Mario", body: "Lorem ipsum..." },
+        { id: 999994, title: "All we know", author: "Charlie", body: "Lorem ipsum..." },
+        { id: 999995, title: "The medbot blogs", author: "Mehmed", body: "Lorem ipsum..." }
     ]);
 
     return (
         <div className="home">
             <BlogLists blogs={blogs} title={'All Blogs'}  />
+            <BlogLists blogs={blogs.filter((blog) => blog.author === 'Mario')} title={"Mario's Blogs"}  />
         </div>
     );
 }
