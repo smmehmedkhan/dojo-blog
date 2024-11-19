5d7a90fce61bf954f859f901b6a4fb9c2d4cbdee
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 00:31:13 +0600

    Declare two state variable using useState from react.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(25);
+
+    function handleClick() {
+        setName('Yushi');
+        setAge(30);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <hr />
+            <p>{ name } is { age } years old.</p>
+            <button onClick={handleClick}>Change</button>
         </div>
     );
 }
