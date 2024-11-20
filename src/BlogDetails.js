b78511b922a5e78de09c0f6e20029a054a19c221
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 18:53:06 +0600

    failed!

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,26 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 export default function BlogDetails() {
     const { id } = useParams();
+    const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
 
     return (
         <div className="blog-details">
-            <h2>Blog details - { id }</h2>
+            { isPending && <div className="loading">
+                    <p>Loading...</p>
+                </div> 
+            }
+
+            { error && <div className="error">{ error }</div> }
+
+            { blog && (
+                <article>
+                    <h2>{ blog.title }</h2>
+                    <h3>Written by { blog.author }</h3>
+                    <p>{ blog.body }</p>
+                </article>
+            )}
         </div>
     );
 }
--- src/useFetch.js
+++ src/useFetch.js
@@ -3,35 +3,33 @@ import { useEffect, useState } from "react";
 export default function useFetch(url) {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         const abortController = new AbortController();
 
-        setTimeout(() => {
-            fetch(url, { signal: abortController.signal})
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error("Didn't find that resource on database.");
-                    } 
-                    return res.json();
-                })
-                .then(d => {
-                    setData(d);
+        fetch(url, { signal: abortController.signal})
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Didn't find that resource on database.");
+                } 
+                return res.json();
+            })
+            .then(d => {
+                setData(d);
+                setIsPending(false);
+                setError(false);
+            })
+            .catch(err => {
+                if (err.name === 'AbortError') {
+                    console.log('Fetch aborted');
+                } else {
+                    setError(err.message)
                     setIsPending(false);
-                    setError(false);
-                })
-                .catch(err => {
-                    if (err.name === 'AbortError') {
-                        console.log('Fetch aborted');
-                    } else {
-                        setError(err.message)
-                        setIsPending(false);
-                    }
-                });
-        }, 1000);
+                }
+            });
 
-        return () => abortController.abort();
+            return () => abortController.abort();
     }, [url]);
 
     return { data, isPending, error };
