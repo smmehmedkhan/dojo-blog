a2e22846f8467a2027bef1ede7c6562581c7e39f
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 23:48:00 +0600

    Add a form field in Create file. Which's tree input field called title, content and author.

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,55 @@
+import { useState } from "react"
+
 export default function Create() {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [author, setAuthor] = useState('mario');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const blog = {
+            title,
+            author,
+            content
+        };
+        console.log(blog);
+    }
+
     return (
         <div className="create">
             <h2>Add a new blog</h2>
+            <form onSubmit={ handleSubmit }>
+                <label>Title:</label>
+                <input 
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required 
+                />
+                <label>Content:</label>
+                <textarea
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    required
+                ></textarea>
+                <label>Author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">Mario</option>
+                    <option value="yushi">Yushi</option>
+                </select>
+                <button>Add blog</button>
+            </form>
+
+            <div className="blog-preview">
+                <article>
+                    <h2>{ title }</h2>
+                    <h3>{ author }</h3>
+                    <p>{ content }</p>
+                </article>
+            </div>
         </div>
     )
 }
