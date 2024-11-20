47789c5f2f969f61c0d70cdf8e4bb5ac269b1f71
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 23:09:56 +0600

    Change style in Css file.

--- src/BlogLists.js
+++ src/BlogLists.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function BlogLists({ blogs, title }) {
     return (
         <>
-            <h2>{ title }</h2>
+            <h2 className="blog-main-title">{ title }</h2>
             { blogs.map((blog) => (
                     <div className="blog-preview" key={blog.id}>
                         <Link to={`/blog/${blog.id}`}>
