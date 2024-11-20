eb9d7cedf0f14bf2b1fa8bd639e0d0a3d207b00a
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 14:46:26 +0600

    Add cath block in fetch method to catch error.

--- src/Home.js
+++ src/Home.js
@@ -4,17 +4,26 @@ import BlogLists from "./BlogLists";
 export default function Home() {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
                 .then(res => {
+                    if (!res.ok) {
+                        throw Error("Didn't find that resource on database.");
+                    } 
                     return res.json();
                 })
                 .then(data => {
                     setBlogs(data);
                     setIsPending(false);
-            });
+                    setError(false);
+                })
+                .catch(err => {
+                    setError(err.massage)
+                    setIsPending(false);
+                })
         }, 1000);
     }, []);
 
@@ -24,6 +33,9 @@ export default function Home() {
                     <p>Loading...</p>
                 </div> 
             }
+
+            { error && <div>{ error }</div> }
+
             { blogs && <BlogLists blogs={blogs} title={'All Blogs'} /> }
         </div>
     );
