5723a2c6f8602271110568683206cae3c208a929
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 18:06:47 +0600

    Add BlogDetails.js file and integrate it's functionality using useParams hooks.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 export default function App() {
   return (
@@ -15,6 +16,9 @@ export default function App() {
           </Route>
           <Route path='/create'>
             <Create />
+          </Route>
+          <Route path='/blog/:id'>
+            <BlogDetails />
           </Route>
         </Switch>
       </div>
