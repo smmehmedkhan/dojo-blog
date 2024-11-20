b218ca9eb78a23d0374cc19d1edd3def137c03e0
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 14:11:32 +0600

    Add conditional loading massage inside home component. So when document fetching data from server, we'll see a loading massage on homepage.

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,28 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import BlogLists from "./BlogLists";
 
 export default function Home() {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                setBlogs(data);
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
             });
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            { isPending && <div>
+                <p>Loading...</p>
+            </div> }
             { blogs && <BlogLists blogs={blogs} title={'All Blogs'} /> }
         </div>
     );
