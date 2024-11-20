05fcb1599c33ef7e97764ab716ea02c41a97fbef
Mehmed Khan <smmehmedkahan@gmail.com>
2024-11-20 17:45:48 +0600

    Added a abort mechanisum in useFetch file to aborted fetching when we pass throw quickly.

--- src/useFetch.js
+++ src/useFetch.js
@@ -6,22 +6,32 @@ export default function useFetch(url) {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch(url)
-            .then(res => {
-                if (!res.ok) {
-                    throw Error("Didn't find that resource on database.");
-                } 
-                return res.json();
-            })
-            .then(d => {
-                setData(d);
-                setIsPending(false);
-                setError(false);
-            })
-            .catch(err => {
-                setError(err.massage)
-                setIsPending(false);
-            });
+        const abortController = new AbortController();
+
+        setTimeout(() => {
+            fetch(url, { signal: abortController.signal})
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error("Didn't find that resource on database.");
+                    } 
+                    return res.json();
+                })
+                .then(d => {
+                    setData(d);
+                    setIsPending(false);
+                    setError(false);
+                })
+                .catch(err => {
+                    if (err.name === 'AbortError') {
+                        console.log('Fetch aborted');
+                    } else {
+                        setError(err.message)
+                        setIsPending(false);
+                    }
+                });
+        }, 1000);
+
+        return () => abortController.abort();
     }, [url]);
 
     return { data, isPending, error };
